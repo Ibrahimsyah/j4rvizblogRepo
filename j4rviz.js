8de1179d3463050b580bc5ebf319ad97cd23adab
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-05-25 08:53:43 +0700

    final

--- j4rviz.js
+++ j4rviz.js
@@ -49,49 +49,48 @@ if (window.innerWidth > 800) {
 var last = 0;
 $(window).scroll(function (event) {
     var st = $(this).scrollTop();
-    if (st & gt; last + 300){
+    if (st &gt; last + 300){
     $("#header-container").css('transform', 'translateY(-48px)');
     last = st;
 }
-    else if (st & lt; last) {
+    else if (st &lt; last) {
     $("#header-container").css('transform', 'translateY(0)');
     last = st;
 }
   });
-
-document.addEventListener(& quot; DOMContentLoaded & quot;, function() {
-    let lazyImages = [].slice.call(document.querySelectorAll(& quot; img.lazy & quot;));
+  document.addEventListener(&quot;DOMContentLoaded&quot;, function() {
+    let lazyImages = [].slice.call(document.querySelectorAll(&quot;img.lazy&quot;));
     let active = false;
-
-    const lazyLoad = function () {
-        if (active === false) {
-            active = true;
-
-            setTimeout(function () {
-                lazyImages.forEach(function (lazyImage) {
-                    if ((lazyImage.getBoundingClientRect().top & lt;= window.innerHeight & amp;& amp; lazyImage.getBoundingClientRect().bottom & gt;= 0) & amp;& amp; getComputedStyle(lazyImage).display !== & quot; none & quot;) {
-                lazyImage.src = lazyImage.dataset.src;
-                lazyImage.srcset = lazyImage.dataset.srcset;
-                lazyImage.classList.remove(& quot; lazy & quot;);
-
-                lazyImages = lazyImages.filter(function (image) {
-                    return image !== lazyImage;
-                });
-
-                if (lazyImages.length === 0) {
-                    document.removeEventListener(& quot; scroll & quot;, lazyLoad);
-                    window.removeEventListener(& quot; resize & quot;, lazyLoad);
-                    window.removeEventListener(& quot; orientationchange & quot;, lazyLoad);
-                }
+  
+    const lazyLoad = function() {
+      if (active === false) {
+        active = true;
+  
+        setTimeout(function() {
+          lazyImages.forEach(function(lazyImage) {
+            if ((lazyImage.getBoundingClientRect().top &lt;= window.innerHeight &amp;&amp; lazyImage.getBoundingClientRect().bottom &gt;= 0) &amp;&amp; getComputedStyle(lazyImage).display !== &quot;none&quot;) {
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.srcset = lazyImage.dataset.srcset;
+              lazyImage.classList.remove(&quot;lazy&quot;);
+  
+              lazyImages = lazyImages.filter(function(image) {
+                return image !== lazyImage;
+              });
+  
+              if (lazyImages.length === 0) {
+                document.removeEventListener(&quot;scroll&quot;, lazyLoad);
+                window.removeEventListener(&quot;resize&quot;, lazyLoad);
+                window.removeEventListener(&quot;orientationchange&quot;, lazyLoad);
+              }
             }
-        });
-
-        active = false;
-    }, 200);
-}
+          });
+  
+          active = false;
+        }, 200);
+      }
     };
-
-document.addEventListener(& quot; scroll & quot;, lazyLoad);
-window.addEventListener(& quot; resize & quot;, lazyLoad);
-window.addEventListener(& quot; orientationchange & quot;, lazyLoad);
+  
+    document.addEventListener(&quot;scroll&quot;, lazyLoad);
+    window.addEventListener(&quot;resize&quot;, lazyLoad);
+    window.addEventListener(&quot;orientationchange&quot;, lazyLoad);
   });
